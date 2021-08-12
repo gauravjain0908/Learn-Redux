cca94901e51aa384c4bd882a69f93f405bf558a9
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-12 16:15:57 +0530

    learn how to create a redux project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+
+
+//Store well the place where we store data 
+
+let store = createStore(counter)
+
+//action
+const increment = () =>
+{
+  return {
+    type: "INCREMENT"
+  }
+} 
+
+const decrement = () => {
+
+  return {
+
+    type: "DECREMENT"
+  }
+}
+//reducer will tell us what is suposed to happen when we perform on a action 
+
+const counter = (state=0, action) =>{
+
+  switch(action.type)
+  {
+    case "INCREMENT" : return state+1;
+    case "DECREMENT" : return state-1;
+  }
+} 
+
+//  
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +47,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
